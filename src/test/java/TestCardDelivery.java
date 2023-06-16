31dc6faffe9f9d7ba9b5d44899b156dd69194c3d
Elena Zimnickaya <nazarova.leno4ka@yandex.ru>
2023-06-16 23:06:11 +0400

    Change test

--- src/test/java/TestCardDelivery.java
+++ src/test/java/TestCardDelivery.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -12,13 +13,16 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class TestCardDelivery {
 
+    @BeforeEach
+    void setup () {open ("http://localhost:9999");}
+
 
 
     @Test
     void shouldDeliveryCard() {
-        open ("http://localhost:9999");
-        $("[data-test-id=city] input").setValue(DataGenerator.generateCity("ru"));
         String firstDate = DataGenerator.generateDate(5);
+        String secondDate = DataGenerator.generateDate(8);
+        $("[data-test-id=city] input").setValue(DataGenerator.generateCity("ru"));
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT,Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id=date] input").sendKeys(firstDate);
         $("[data-test-id=name] input").setValue(DataGenerator.generateName("ru"));
@@ -27,11 +31,10 @@ public class TestCardDelivery {
         $("button.button").click();
         $("[data-test-id=notification")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Успешно! Встреча успешно забронирована на " + firstDate));
+                .shouldHave(Condition.exactText("Успешно! Встреча успешно запланирована на " + firstDate));
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT,Keys.HOME), Keys.BACK_SPACE);
-        String secondDate = DataGenerator.generateDate(8);
         $("[data-test-id=notification")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Успешно! Встреча успешно забронирована на " + secondDate));
+                .shouldHave(Condition.exactText("Успешно! Встреча успешно запланирована на " + secondDate));
     }
 }
